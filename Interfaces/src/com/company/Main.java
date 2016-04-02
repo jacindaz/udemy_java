6cf784af7e536d1345e5e71f6378e96534c02b1d
Jacinda <jacindaz@gmail.com>
2016-04-02 13:15:49 -0400

    pass in playlist ArrayList into WestCoastSwing instance
     - change name of printArrayList method, since really
       only using it to print the playlist
       (is the Main class the right place for it? not sure where
         else to put it)

--- Interfaces/src/com/company/Main.java
+++ Interfaces/src/com/company/Main.java
@@ -5,17 +5,23 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-        WestCoastSwing someDance = new WestCoastSwing();
-//        WestCoastSwing someMusic = new WestCoastSwing();
+        ArrayList<String> playlist = new ArrayList<String>();
+        playlist.add("Sorry");
+        playlist.add("The Other");
+
+        WestCoastSwing someDance = new WestCoastSwing(playlist);
 
         someDance.spin(3);
         someDance.playSong("Sorry");
-        printArrayList(someDance.getPlaylist());
+        printPlaylist(someDance.getPlaylist());
     }
 
-    public static void printArrayList(ArrayList<String> list) {
+    public static void printPlaylist(ArrayList<String> list) {
+        System.out.println("\n==========");
+
         for(String item : list){
-            System.out.println(item + "\n");
+            System.out.println("Song number " +(list.indexOf(item) + 1) + ": "  + item + "");
         }
+        System.out.println("==========\n");
     }
 }
